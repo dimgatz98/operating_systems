5c06503351a278c5f4ab1cb4e8f6f670524dff57
Tzourhs <jimaras20111@gmail.com>
2021-05-30 17:45:38 +0300

    teliko arxeio ths askhshsh 4

--- 4/ask_4.c
+++ 4/ask_4.c
@@ -7,32 +7,40 @@
 #include <getopt.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define MAX_LIMIT 50
 
-int main(int argc, char **argv)
-{
-    if(argc > 4){
-        perror("Too many arguments!\n");
-        exit(0);
+void remove_front_zeros(char *arr){
+    int i = 0;
+    while(arr[0] == '0'){    
+        while(arr[i] != '\0'){
+            arr[i] = arr[i + 1];
+            i++;
+        }
+        i = 0;
     }
-    
+}
+
+int main(int argc, char **argv){
     time_t timer = time(NULL);
     struct tm *tm_info = localtime(&timer);
     char *address, datetime[26];
     strftime(datetime, 26, "%Y-%m-%d %H:%M:%S", tm_info);
-    int i = 0, PORT = 18080, opt, debug = 0, option_index = 0,sock = 0, valread;
-    struct option long_opt[] ={
+    int PORT = 18080, opt, debug = 0, option_index = 0,sock = 0, valread;
+    
+    struct option long_opt[] = {
         {"host", required_argument, NULL, 0},
         {"port", required_argument, NULL, 1},
         {"debug", no_argument, NULL, 2}
     }; 
+    
     struct hostent *hostnm;
     struct sockaddr_in serv_addr;
     char msg[MAX_LIMIT], temp1[4], temp2[6], buffer[1024];
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
+    
+    if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Socket creation error \n");
+        exit(-1);
     }
    
     hostnm = gethostbyname("lab4-server.dslab.os.grnetcloud.net");
@@ -45,11 +53,15 @@ int main(int argc, char **argv)
         switch(opt){
             case 0:
                 address = optarg;
-                hostnm = gethostbyname(address);
-                serv_addr.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
+                
+                if(inet_pton(AF_INET, address, &serv_addr.sin_addr) <= 0){
+                    hostnm = gethostbyname(address);
+                    serv_addr.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
+                }
                 break;
             case 1:
-                PORT = *optarg;
+                PORT = atoi(optarg);
+                serv_addr.sin_port = htons(PORT);
                 break;
             case 2:
                 debug = 1;
@@ -57,17 +69,20 @@ int main(int argc, char **argv)
         }
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
         printf("\nConnection Failed \n");
-        return -1;
+        exit(-1);
     }
 
     while(1){
         fgets(msg, MAX_LIMIT, stdin);
         msg[strlen(msg)-1] = 0;
-        send(sock , msg , strlen(msg) , 0);
-        valread = read(sock , buffer, 1024);//recv(2)
+        if(strcmp(msg, "exit") == 0){
+            close(sock);
+            exit(0);
+        }
+        send(sock , msg , strlen(msg) , 0);//when no flags equivalent to write(2);
+        valread = read(sock , buffer, 1024);//equivalent to recv(2);
         buffer[valread - 1] = 0;
         if(debug == 1){
             printf("[DEBUG] sent '%s'\n", msg);
@@ -76,23 +91,12 @@ int main(int argc, char **argv)
         if(strcmp(msg, "help") == 0){
             printf("%s\n", buffer);
         }
-        else if(strcmp(msg, "exit") == 0){
-            //printf("exiting\n");
-            close(sock);
-            exit(0);
-        }
         else if(strcmp(msg, "get") == 0){
             temp1[0] = buffer[2];
             temp1[1] = buffer[3];
             temp1[2] = buffer[4];
             temp1[3] = 0;
-            while(temp1[0] == '0'){    
-                while(temp1[i] != '\0'){
-                    temp1[i] = temp1[i + 1];
-                    i++;
-                }
-                i = 0;
-            }
+            remove_front_zeros(temp1);
 
             temp2[0] = buffer[6];
             temp2[1] = buffer[7];
@@ -100,13 +104,7 @@ int main(int argc, char **argv)
             temp2[3] = buffer[8];
             temp2[4] = buffer[9];
             temp2[5] = 0;
-            while(temp2[0] == '0'){    
-                while(temp2[i] != '\0'){
-                    temp2[i] = temp2[i + 1];
-                    i++;
-                }
-                i = 0;
-            }
+            remove_front_zeros(temp2);
 
             printf("Latest event:\n");
             switch(buffer[0]){
@@ -136,7 +134,7 @@ int main(int argc, char **argv)
             printf("Send verification code: %s\n", buffer);
         }
         else{
-            printf("Response:'%s'", buffer);
+            printf("Response:'%s'\n", buffer);
         }
         memset(buffer, 0, strlen(buffer));
     }
